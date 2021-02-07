5efc7df64a56748befbcdf9416f3645650e5f246
Cian O'Gralaigh <cianogralaigh@Cians-MacBook-Pro.local>
2021-02-07 16:58:46 +0000

    Add first page

--- src/com/cian/servlet/HelloWorld.java
+++ src/com/cian/servlet/HelloWorld.java
@@ -15,14 +15,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HelloWorld() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloWorld() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -35,15 +35,16 @@ public class HelloWorld extends HttpServlet {
 		out.println("<h1>Hello World</h1>");
 		out.println("<hr/>");
 		out.println("time on the server is: " + new java.util.Date());
-
 		out.println("</html></body>");
 
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
